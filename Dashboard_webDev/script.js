5610806d5546049d6a9f94abf7b2cb90fb61eb24
Sultan Zaki <149162594+zackyhermawan@users.noreply.github.com>
2024-12-12 07:08:14 +0700

    Update script.js

--- Dashboard_webDev/script.js
+++ Dashboard_webDev/script.js
@@ -1,7 +1,8 @@
-const arrow = document.getElementById("arrow-button");
-const sidebar = document.getElementById("sidebar");
 const profil = document.getElementById("profil");
 const popup = document.getElementById("popup");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const arrow = document.getElementById("arrow-button");
+const sidebar = document.getElementById("sidebar");
 
 arrow.addEventListener("click", function () {
   sidebar.classList.toggle("hide");
@@ -10,4 +11,14 @@ arrow.addEventListener("click", function () {
 
 profil.addEventListener("click", function () {
   popup.classList.toggle("open-profil");
+});
+
+hamburgerMenu.addEventListener("click", function () {
+  sidebar.classList.toggle("open-navigation");
+});
+
+document.addEventListener("click", function (event) {
+  if (!sidebar.contains(event.target) && event.target !== hamburgerMenu) {
+    sidebar.classList.remove("open-navigation");
+  }
 });
