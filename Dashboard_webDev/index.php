d582db656252869db643d750a1c2cb9e750daa83
Sultan Zaki <149162594+zackyhermawan@users.noreply.github.com>
2024-12-13 13:29:59 +0700

    Update and rename index.html to index.php

--- Dashboard_webDev/index.php
+++ Dashboard_webDev/index.php
@@ -1,3 +1,28 @@
+<?php
+include 'database/database.php';
+session_start();
+$error = "";
+if (isset($_POST['login'])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $db->query($sql);
+
+    if ($result->num_rows > 0) {
+        $account = $result->fetch_assoc();
+        $_SESSION["username"] = $data["username"];
+        $_SESSION["isLogin"] = true;
+        header('Location: dashboard.php');
+        exit();
+    } else{
+    $error = "Username atau password salah.";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,14 +31,20 @@
     <title>Login - Inventory Grocery</title>
     <link rel="stylesheet" href="style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   </head>
   <body>
     <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
-      <h4 class="text-center mb-5">Inventory Grocery Store</h4>
-      <div class="card shadow-sm p-5 border-0" style="width: 500px; background-color: #f8fafc">
-        <div class="card-body">
+      <div class="card card-login shadow-sm p-4 border-0" style="width: 40%; background-color: #f8fafc">
+        <img src="asset/logo-login.png" class="mx-auto d-block" width=150 alt="">
+        <h4 class="text-center ">Inventory Grocery Store</h4>
+        <p class="text-center fw-semibold fs-4">Sign in</p>
+          <div class="card-body">
+          <?php if ($error): ?>
+            <p style="color: red;"><?php echo $error;?></p>
+          <?php endif; ?>
           <form method="POST">
             <div class="form-group mb-3">
               <label for="username" class="fw-semibold">Username</label>
@@ -25,7 +56,7 @@
               <input type="password" class="login form-control border-0" id="password" name="password" required />
               <div class="border border-dark"></div>
             </div>
-            <button type="submit" class="btn btn-primary mt-5 w-100 btn-block">Login</button>
+            <button type="submit" name="login" class="btn btn-primary mt-5 w-100 btn-block">Login</button>
           </form>
         </div>
       </div>
