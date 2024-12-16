067c97808202779855735275e26c8b9fdcd3f948
Sultan Zaki <149162594+zackyhermawan@users.noreply.github.com>
2024-12-16 17:24:59 +0700

    Update barangMasuk.php

--- Dashboard_webDev/barangMasuk.php
+++ Dashboard_webDev/barangMasuk.php
@@ -1,10 +1,50 @@
 <?php
 include "database/database.php";
 session_start();
+
+// PAGGINATION
+include "php/paggination.php";
+
+$sql_view_masuk = "SELECT 
+                  produk.id_produk, 
+                  produk.nama_produk,
+                  produk.stok,
+                  produk_masuk.jumlah_masuk, 
+                  produk_masuk.created_at 
+                  FROM produk_masuk 
+                  INNER JOIN produk 
+                  ON produk_masuk.id_produk = produk.id_produk ORDER BY created_at DESC LIMIT $limit_baris OFFSET $offset";
+
+$result_view_masuk = mysqli_query($db, $sql_view_masuk);
+
+
+// SELECT TAMBAH STOK
+$sql_tambah_stok = "SELECT id_produk, nama_produk, stok FROM produk";
+$result_tambah_stok = mysqli_query($db, $sql_tambah_stok);
+// TAMBAH STOK
+if (isset($_POST['tambah'])) {
+  $id_produk = $_POST['id_produk'];
+  $jumlah_stok = $_POST['tambah_stok'];
+  $sql_nama_produk = "SELECT nama_produk FROM produk WHERE id_produk = '$id_produk'";
+  $result_nama_produk = mysqli_query($db, $sql_nama_produk);
+
+  if ($selected_produk = mysqli_fetch_assoc($result_nama_produk)) {
+    $nama_produk = $selected_produk['nama_produk'];
+    $sql_tambah = "INSERT INTO produk_masuk (id_produk, jumlah_masuk, created_at) 
+                   VALUES ('$id_produk', '$jumlah_stok', NOW())";
+    $result_tambah = mysqli_query($db, $sql_tambah);
+
+    // TAMBAH STOK DI TB. PRODUK
+    $sql_update_produk = "UPDATE produk SET stok = stok + '$jumlah_stok' WHERE id_produk = '$id_produk'";
+    if ($result_update_produk = mysqli_query($db, $sql_update_produk)) {
+      header('location: barangMasuk.php');
+    }
+  }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en" data-bs-theme="light">
+<html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -12,12 +52,11 @@ session_start();
     <link rel="stylesheet" href="style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   </head>
   <body>
     <div class="d-flex main">
-      <aside id="sidebar" class="sidebar pt-2 ">
+      <aside id="sidebar" class="sidebar cards pt-2 ">
         <div class="list-group mt-3">
         <a href="" class="close-btn p-1"><i class="bi bi-x-lg text-white fs-5 mx-2"></i></a>
         <div class="d-flex mb-4 align-items-center">
@@ -35,8 +74,8 @@ session_start();
           </a>
           <div class="kelolastok-dropdown collapse" id="kelolaProduk">
             <ul class="list-unstyled d-flex align-items-center flex-column gap-2">
-              <li class="d-flex align-items-center my-2"><a href="barangMasuk.php" class="menu barangMasuk-aside p-1 nav-link text-white rounded"><svg class="icon-dropdown"version='1.1'id='Layer_1' xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'viewBox='0 0 122.88 94.45'style='enable-background: new 0 0 122.88 94.45'xml:space='preserve'width='20'height='20'fill='white'><g><path class='st0'd='M0,41.16h24.83v44.18H0V41.16L0,41.16z M36,11.76L71.01,0.1c0.42-0.14,0.86-0.13,1.24,0.01V0.1l35.47,12.15 c0.87,0.3,1.4,1.14,1.32,2.02c0.01,0.04,0.01,0.09,0.01,0.14v37.04l1.56-0.77c9.6-3.16,16.43,6.88,9.35,13.87 c-13.9,10.11-28.15,18.43-42.73,25.15c-10.59,6.44-21.18,6.22-31.76,0l-15.63-8.07V44.71h4.48V13.7 C34.31,12.71,35.04,11.89,36,11.76L36,11.76z M46.44,44.71c7.04,1.26,14.08,5.08,21.12,9.51h1.47V33.88L38.97,21.05v23.66H46.44 L46.44,44.71z M74.43,54.22h6.04c5.84,0.35,8.9,6.27,3.22,10.16c-2.67,1.96-5.84,2.7-9.26,2.86v4.89 C80.83,71.77,86.1,70,89.49,64.7l1.93-4.51l12.97-6.43V20.78L74.43,33.9V54.22L74.43,54.22z M69.04,67.12 c-0.65-0.05-1.31-0.1-1.96-0.16c-4.22-0.21-4.4,5.46,0,5.48c0.64,0.05,1.3,0.02,1.96-0.04v-1.5V67.12L69.04,67.12z M71.6,5.49 l-29.82,9.94l29.96,13.59l29.97-13.21L71.6,5.49L71.6,5.49z'/></g></svg><span class="submenu-dropdown">Barang Masuk</span></a></li>
-              <li class="d-flex align-items-center"><a href="barangKeluar.php" class="menu p-1 nav-link text-white rounded"><svg class="icon-dropdown" fill="white" width='20' height='20' xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 505 511.5"><path d="m336.11 39.84-115.38 68.94 135.38 18.4 111.32-69.44-131.32-17.9zm25.45 204.61c73.74 0 133.53 59.78 133.53 133.53 0 73.74-59.79 133.52-133.53 133.52-73.75 0-133.53-59.78-133.53-133.52 0-73.75 59.78-133.53 133.53-133.53zm-50.44 179.72 15.51-78.82 15.73 23.69c33.86-13.59 52.88-36 55.7-70.5 27.82 48.63 10.93 92.22-24.33 117.77l16.05 24.16-78.65-16.3h-.01zM204.83 126.13l-.09 141.71-51.45-35.04-51.46 29.07 6.1-148.91-88.54-12.03v312.98l178.95 23.13c2.52 7.1 5.47 13.99 8.85 20.63L9.3 432.07c-5.17-.2-9.3-4.47-9.3-9.68V89.86c.27-4.05 1.89-6.89 5.72-8.81L182.48.85c1.58-.72 3.52-1.01 5.25-.77l308.18 42.04c5.09.59 8.58 4.77 8.58 9.99v.02L505 280.9c-5.72-8.46-15.57-20.29-19.93-27.77V69.56l-115.81 74.93v59.81a174.846 174.846 0 0 0-19.39.36v-58.82l-145.04-19.71zm-81.52-30.58 112.17-69.44-47.58-6.49L44.24 84.8l79.07 10.75z"/></svg><span class="submenu-dropdown">Barang Keluar</span></a></li>
+              <li class="d-flex align-items-center my-2"><a href="barangMasuk.php" class="menu d-flex p-1 nav-link text-white rounded"><svg class="icon-dropdown"version='1.1'id='Layer_1' xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'viewBox='0 0 122.88 94.45'style='enable-background: new 0 0 122.88 94.45'xml:space='preserve'width='20'height='20'fill='white'><g><path class='st0'd='M0,41.16h24.83v44.18H0V41.16L0,41.16z M36,11.76L71.01,0.1c0.42-0.14,0.86-0.13,1.24,0.01V0.1l35.47,12.15 c0.87,0.3,1.4,1.14,1.32,2.02c0.01,0.04,0.01,0.09,0.01,0.14v37.04l1.56-0.77c9.6-3.16,16.43,6.88,9.35,13.87 c-13.9,10.11-28.15,18.43-42.73,25.15c-10.59,6.44-21.18,6.22-31.76,0l-15.63-8.07V44.71h4.48V13.7 C34.31,12.71,35.04,11.89,36,11.76L36,11.76z M46.44,44.71c7.04,1.26,14.08,5.08,21.12,9.51h1.47V33.88L38.97,21.05v23.66H46.44 L46.44,44.71z M74.43,54.22h6.04c5.84,0.35,8.9,6.27,3.22,10.16c-2.67,1.96-5.84,2.7-9.26,2.86v4.89 C80.83,71.77,86.1,70,89.49,64.7l1.93-4.51l12.97-6.43V20.78L74.43,33.9V54.22L74.43,54.22z M69.04,67.12 c-0.65-0.05-1.31-0.1-1.96-0.16c-4.22-0.21-4.4,5.46,0,5.48c0.64,0.05,1.3,0.02,1.96-0.04v-1.5V67.12L69.04,67.12z M71.6,5.49 l-29.82,9.94l29.96,13.59l29.97-13.21L71.6,5.49L71.6,5.49z'/></g></svg><p class="submenu-dropdown m-0">Barang Masuk</p></a></li>
+              <li class="d-flex align-items-center"><a href="barangKeluar.php" class="menu p-1 d-flex nav-link text-white rounded"><svg class="icon-dropdown" fill="white" width='20' height='20' xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 505 511.5"><path d="m336.11 39.84-115.38 68.94 135.38 18.4 111.32-69.44-131.32-17.9zm25.45 204.61c73.74 0 133.53 59.78 133.53 133.53 0 73.74-59.79 133.52-133.53 133.52-73.75 0-133.53-59.78-133.53-133.52 0-73.75 59.78-133.53 133.53-133.53zm-50.44 179.72 15.51-78.82 15.73 23.69c33.86-13.59 52.88-36 55.7-70.5 27.82 48.63 10.93 92.22-24.33 117.77l16.05 24.16-78.65-16.3h-.01zM204.83 126.13l-.09 141.71-51.45-35.04-51.46 29.07 6.1-148.91-88.54-12.03v312.98l178.95 23.13c2.52 7.1 5.47 13.99 8.85 20.63L9.3 432.07c-5.17-.2-9.3-4.47-9.3-9.68V89.86c.27-4.05 1.89-6.89 5.72-8.81L182.48.85c1.58-.72 3.52-1.01 5.25-.77l308.18 42.04c5.09.59 8.58 4.77 8.58 9.99v.02L505 280.9c-5.72-8.46-15.57-20.29-19.93-27.77V69.56l-115.81 74.93v59.81a174.846 174.846 0 0 0-19.39.36v-58.82l-145.04-19.71zm-81.52-30.58 112.17-69.44-47.58-6.49L44.24 84.8l79.07 10.75z"/></svg><p class="submenu-dropdown m-0">Barang Keluar</p></a></li>
             </ul>
           </div>
           <a href="laporan.php" class="menu text-decoration-none mt-4 p-1 mx-2 rounded" aria-current="true"><i class="bi bi-file-earmark-text-fill fs-5 mx-2"></i><span class="position-absolute">Laporan</span></a>
@@ -110,67 +149,35 @@ session_start();
                         <thead>
                           <tr>
                             <th style="width: 30px;">No</th>
+                            <th>Kode Produk</th>
                             <th>Nama Produk</th>
-                            <th>Tanggal</th>
                             <th>Jumlah</th>
-                            <th>Aksi</th>
+                            <th>Tanggal</th>
                           </tr>
                         </thead>
                         <tbody>
+                        <?php
+                          $no = $offset +1;
+                          while ($data = mysqli_fetch_array($result_view_masuk)):
+                            
+                          ?>
                               <tr>
-                                <th>1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>Otto</td>
-                                <td>
-                                  <div class='d-flex gap-2'>
-                                    <a data-bs-toggle='modal' data-bs-target='#edit' class='fs-5 text-white d-flex align-items-center justify-content-center bg-primary rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-pencil-square'></i
-                                    ></a>
-                                    <a data-bs-toggle='modal' data-bs-target='#delete' class='fs-5 text-white d-flex align-items-center justify-content-center bg-danger rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-trash'></i
-                                    ></a>
-                                  </div>
-                                </td>
+                                <td><?=$no++?></td>
+                                <td><?=$data['id_produk']?></td>
+                                <td><?=$data['nama_produk']?></td>
+                                <td><?=$data['jumlah_masuk']?></td>
+                                <td><?=$data['created_at']?></td>
                               </tr>
-                              <tr>
-                                <th>2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>Thornton</td>
-                                <td>
-                                  <div class='d-flex gap-2'>
-                                    <a data-bs-toggle='modal' data-bs-target='#edit' class='fs-5 text-white d-flex align-items-center justify-content-center bg-primary rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-pencil-square'></i
-                                    ></a>
-                                    <a data-bs-toggle='modal' data-bs-target='#delete' class='fs-5 text-white d-flex align-items-center justify-content-center bg-danger rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-trash'></i
-                                    ></a>
-                                  </div>
-                                </td>
-                              </tr>
-                              <tr>
-                                <th>3</th>
-                                <td>Larry the Bird</td>
-                                <td>Larry the Bird</td>
-                                <td>Larry the Bird</td>
-                                <td>
-                                  <div class='d-flex gap-2'>
-                                    <a data-bs-toggle='modal' data-bs-target='#edit' class='fs-5 text-white d-flex align-items-center justify-content-center bg-primary rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-pencil-square'></i
-                                    ></a>
-                                    <a data-bs-toggle='modal' data-bs-target='#delete' class='fs-5 text-white d-flex align-items-center justify-content-center bg-danger rounded' style='width: 30px; height: 30px'
-                                      ><i class='bi bi-trash'></i
-                                    ></a>
-                                  </div>
-                                </td>
-                              </tr>
+                              
+                            </tbody>
+                            <?php endwhile ?>
                             </tbody>
                       </table>
                     </div>
                     <div class="change-page d-flex gap-2 align-items-center justify-content-center">
-                      <a href="" class="p-1 d-flex align-items-center justify-content-center fw-semibold bg-primary text-decoration-none text-white rounded" style="width: 35px; height: 35px">1</a>
-                      <a href="" class="p-1 d-flex align-items-center justify-content-center fw-semibold text-decoration-none text-dark rounded" style="width: 35px; height: 35px; background-color: var(--hover)">2</a>
+                      <?php for($i = 1; $i <= $total_page; $i++): ?>
+                      <a href="?page=<?= $i?>" class="p-1 d-flex align-items-center justify-content-center fw-semibold  text-decoration-none rounded <?= $i == $page? 'bg-primary text-white':'bg-secondary text-white'?>" style="width: 35px; height: 35px"><?= $i ?></a>
+                      <?php endfor ?>
                     </div>
                   </div>
                 </div>
@@ -179,12 +186,40 @@ session_start();
         </div>
       </div>
     </div>
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="script.js"></script>
     <script src="chart.js"></script>
   </body>
+  <!-- Modal ADD DATA -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Catat barang masuk</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <!-- FORM ADD DATA -->
+          <form method="POST" class="p-4 d-flex flex-column rounded-3 justify-content-center">
+              <select class="form-control" name="id_produk">
+                <?php while ($select = mysqli_fetch_array($result_tambah_stok)):?>
+                <option value="<?= $select['id_produk'] ?>"><?= $select['nama_produk'] ?></option>
+                <?php endwhile ?>
+              </select>
+              <br>
+              
+              <div class="mb-3">
+                <input type="number" name="tambah_stok" placeholder="Stok Awal" class="form-control" id="" required />
+              </div>
+              <br>
+              
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button name="tambah" class="btn btn-primary">Add changes</button>
+            </div>
+        </form>
+        </div>
+    </div>
+  </div>
 </html>
